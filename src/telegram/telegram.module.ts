83bc7943888382f43dee0adfb974084b79eafbd9
thesaddest <artsemshauchuk@gmail.com>
2025-09-13 09:07:42 +0200

    [SB]: Switch to webhook V2

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '../config/config.service';
+import { TelegramController } from './telegram.controller';
 import { TelegramService } from './telegram.service';
 
 @Module({
+  controllers: [TelegramController],
   providers: [TelegramService, ConfigService],
   exports: [TelegramService],
 })
